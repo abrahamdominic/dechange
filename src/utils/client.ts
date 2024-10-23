e720c557c774ecb1f6d153ab6dd8380b04014e94
Melasin | AmblessedCT <121829314+melasin1237@users.noreply.github.com>
2024-10-23 13:47:39 +0100

    Added the SoulBOundTokenABI.json file.

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,15 +1,16 @@
-// client.ts
+
 import web3 from './web3'; 
 import deChangeABI from './DeChangeABI.json'; 
-import soulBoundTokenABI from './SoulBoundTokenABI.json'
+import soulBoundTokenABI from './SoulBoundTokenABI.json'; 
 import { AbiItem } from 'web3-utils';
 
 // Address variables
 const deChangeAddress = '0xbA43C9d6D80b92AB28eac94D24B923C18eE48dB8';
 const soulBoundTokenAddress = '0xD0957D9219EB60Af476D86b240650BB6785b1599';
 
-// Initialize contracts
-export const deChangeContract = new web3.eth.Contract(deChangeABI as unknown as AbiItem[], deChangeAddress);
-export const soulBoundTokenContract = new web3.eth.Contract(soulBoundTokenABI as unknown as AbiItem[], soulBoundTokenAddress);
+// Ensure the ABIs are cast correctly as AbiItem[]
+const deChangeContract = new web3.eth.Contract(deChangeABI as AbiItem[], deChangeAddress);
+const soulBoundTokenContract = new web3.eth.Contract(soulBoundTokenABI as AbiItem[], soulBoundTokenAddress);
 
+export { deChangeContract, soulBoundTokenContract };
 export default web3;
